b1200513425f71ad02a2501776205c9b959b9cba
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-03-28 13:05:38 -0300

    atividade feita

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,54 @@
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref } from 'vue';
 
 
-const usuario = reactive({
-nome: "",
-email: '',
-idade: '',
-})
 
-function verificacao(usuario) {
-    if (usuario.nome.length >= 3 && usuario.nome.length <= 20 && usuario.idade.value >= 18 && usuario.idade.value <= 60) {
-        usuario.nome = "";
-        usuario.email = '';
-        usuario.idade = 0;
+ const nome = ref('');
+ const email = ref('');
+ const idade =  ref(0) ;
+const msg = ref(false);
 
+function verificacao() {
+    if (nome.value.length >=3
+        && nome.value.length <= 20
+        && idade.value >= 18
+        && idade.value <= 60
+        && email.value != '') {
+        msg.value = true
     }
 }
 </script>
 
 <template>
-    <form @submit.prevent="verificacao(usuario)">
-        <input type="text" v-model.trim.lazy="nome" placeholder="Digite seu nome" required />
+    <form @submit.prevent="verificacao">
+    <div>
+        <label for="inputName">Nome</label>
+        <input type="text" v-model.trim.lazy="nome" placeholder="Digite seu nome" id="inputName" required />
         <p>Nome digitado é: {{ nome }} </p>
-        <input type="email" v-model.trim.lazy="email" placeholder="Digite seu email" require>
+    </div>
+    <div>
+        <label for="inputEmail">Email</label>
+        <input id="inputEmail" type="email" v-model.trim.lazy="email" placeholder="Digite seu email" require>
         <p>O email digitado é: {{ email }}</p>
-        <input type="number" v-model.trim.number="idade" placeholder="Digite sua Idade " required>
+    </div>
+    <div>
+        <label for="inputAge">Idade</label>
+        <input id="inputAge" type="number" v-model.trim.number="idade" placeholder="Digite sua Idade " required>
         <p>A idade digitada é: {{ idade }}</p>
-        <button @click="verificacao(usuario)" >enviar</button>
+    </div>
+        <div>
+        <button type="submit">enviar</button>
+        </div>
     </form>
+     <div v-show="msg">
+    <p>confirmação registro</p>
+    <div>
+        <p>Nome: {{ nome }}</p>
+        <p>Idade: {{ idade }}</p>
+        <p>Email: {{ email }}</p>
+    </div>
+    </div>
+
 </template>
 
 <style></style>
